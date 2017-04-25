1f654799c38cec24dc1998646a4fa84388e0b534
Carsten Düllmann <cadull@microsoct.com>
2017-04-26 00:29:56 +0200

    wasting perf

--- src/web/Controllers/FunctionsController.cs
+++ src/web/Controllers/FunctionsController.cs
@@ -18,22 +18,39 @@ namespace funcApp.Controllers
         {
             // deliberately commiting the fat controller antipattern
         
-            var list = new List<Tuple<double,double>>();
             var x = squareFunctionLowerBound;
             var interval = squareFunctionInterval >= 0 ? squareFunctionInterval : 1.0;
             var upper = squareFunctionUpperBound >= squareFunctionLowerBound ? squareFunctionUpperBound : squareFunctionLowerBound;
 
-            while (x <= squareFunctionUpperBound)
-            {
-                list.Add(new Tuple<double,double>(x, Math.Pow(x, 2)));
-                x += interval;
-            }
-
             var functions = new FunctionsModel();
-            functions.SquareFunctionInterval = squareFunctionInterval;
-            functions.SquareFunctionLowerBound = squareFunctionLowerBound;
-            functions.SquareFunctionUpperBound = squareFunctionUpperBound;
-            functions.Values = list;
+            
+            // wasting a bit of performance
+
+            for (int i = 0; i<10000; i++)
+            {
+                var list = new List<Tuple<double,double>>();
+                while (x <= squareFunctionUpperBound)
+                {
+                    list.Add(new Tuple<double,double>(x, Math.Pow(x, 2)));
+                    x += interval;
+                }
+
+                functions.SquareFunctionInterval = squareFunctionInterval;
+                functions.SquareFunctionLowerBound = squareFunctionLowerBound;
+                functions.SquareFunctionUpperBound = squareFunctionUpperBound;
+                functions.Values = list;
+
+                // wasting more performance
+                try
+                {
+                    throw new Exception($"Blah {i}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
 
             return View(functions);
         }
