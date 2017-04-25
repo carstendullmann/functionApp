61f6e0b4bffb1796783b402e9d629a02a302507c
Carsten Düllmann <cadull@microsoct.com>
2017-04-26 01:02:13 +0200

    fix

--- src/web/Controllers/FunctionsController.cs
+++ src/web/Controllers/FunctionsController.cs
@@ -18,16 +18,16 @@ namespace funcApp.Controllers
         {
             // deliberately commiting the fat controller antipattern
         
-            var x = squareFunctionLowerBound;
-            var interval = squareFunctionInterval >= 0 ? squareFunctionInterval : 1.0;
-            var upper = squareFunctionUpperBound >= squareFunctionLowerBound ? squareFunctionUpperBound : squareFunctionLowerBound;
-
             var functions = new FunctionsModel();
             
             // wasting a bit of performance
 
             for (int i = 0; i<10000; i++)
             {
+                var x = squareFunctionLowerBound;
+                var interval = squareFunctionInterval >= 0 ? squareFunctionInterval : 1.0;
+                var upper = squareFunctionUpperBound >= squareFunctionLowerBound ? squareFunctionUpperBound : squareFunctionLowerBound;
+
                 var list = new List<Tuple<double,double>>();
                 while (x <= squareFunctionUpperBound)
                 {
